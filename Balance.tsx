06c9f9f79a6d0d107b142ec7f99082b85e456c25
Dick Larsson <dicklarsson@gmail.com>
2023-05-27 19:34:50 +0200

    Go back to name Rebel Wallet

--- Balance.tsx
+++ Balance.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function Balance({ balance }: { balance: number }) {
   return (
     <article> 
-      <h1 className="headline">Ravencoin wallet</h1>
-      <h5>Balance</h5>
+      <h1 className="headline">Rebel wallet</h1>
+      <h5>Ravencoin balance</h5>
       <p>{balance.toLocaleString()} RVN</p>
     </article>
   );
