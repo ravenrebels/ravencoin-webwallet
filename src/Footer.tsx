82dbd43f965847260d8664f3f52a0e8271ed77db
Dick Larsson <dicklarsson@gmail.com>
2023-06-29 12:29:49 +0200

    Background now gradient of Ravencoin colors. Update footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,33 +1,39 @@
 import React from "react";
 
-export function Footer({signOut}) {
+export function Footer({ signOut }) {
   return (
-    <div> 
-      <div className="grid">
-        <button onClick={signOut}>Sign out</button>
-        <button
-          className="secondary"
-          onClick={(event) => {
-            const value = localStorage.getItem("mnemonic");
-            if (value) {
-              const target = event.target as HTMLButtonElement;
-              navigator.clipboard.writeText(value);
-              target.disabled = true;
-              setInterval(() => (target.disabled = false), 2000);
-            }
-          }}
-        >
-          Copy your secret 12 words to memory
-        </button>
-      </div>
+    <article>
       <p>
-        <a href="https://github.com/ravenrebels/ravencoin-webwallet">
-          Open source on GitHub
+        Brought to you by{" "}
+        <a href="https://twitter.com/RavenRebels" target="_blank">
+          Raven Rebels
         </a>
-        <br />
-        Icons from{" "}
-        <a href="https://feathericons.com/">https://feathericons.com/</a>
       </p>
-    </div>
+      <a href="https://github.com/ravenrebels/ravencoin-webwallet">
+        Open source on GitHub
+      </a>
+      <br />
+      Icons from{" "}
+      <a href="https://feathericons.com/">https://feathericons.com/</a>
+      <footer>
+        <div className="grid">
+          <button onClick={signOut}>Sign out</button>
+          <button
+            className="secondary"
+            onClick={(event) => {
+              const value = localStorage.getItem("mnemonic");
+              if (value) {
+                const target = event.target as HTMLButtonElement;
+                navigator.clipboard.writeText(value);
+                target.disabled = true;
+                setInterval(() => (target.disabled = false), 2000);
+              }
+            }}
+          >
+            Copy your secret 12 words to memory
+          </button>
+        </div>
+      </footer>
+    </article>
   );
 }
