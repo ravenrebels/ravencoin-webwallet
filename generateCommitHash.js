41176d94ddda318ab12f9546a34f1f896126da1d
Dick Larsson <dicklarsson@gmail.com>
2023-11-28 14:17:49 +0100

    Commit hash version function must create dist folder if needed

--- generateCommitHash.js
+++ generateCommitHash.js
@@ -1,10 +1,20 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
+const path = require('path');
+
+const distDir = path.join(__dirname, 'dist'); // Adjust the path as needed for your project structure
+const commitHashFilePath = path.join(distDir, 'commitHash.js');
 
 try {
+    // Ensure the dist directory exists, create it if necessary
+    fs.mkdirSync(distDir, { recursive: true });
+
+    // Get the commit hash
     const commitHash = execSync('git rev-parse HEAD').toString().trim();
     const content = `const COMMIT_HASH = '${commitHash}';\n`;
-    fs.writeFileSync('dist/commitHash.js', content);
+
+    // Write the commit hash to the file
+    fs.writeFileSync(commitHashFilePath, content);
 } catch (error) {
     console.error('Failed to generate commit hash:', error);
 }
